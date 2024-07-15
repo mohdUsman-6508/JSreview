0059d8e1551248ac8b784692350efb09371dd389
Mohammad Usman <uk351675@gmail.com>
2024-07-15 15:54:10 +0530

    day2

---
+++ .devcontainer/30daysChallenge/day2.js
@@ -0,0 +1,103 @@
+// Day 2: Operators
+// Tasks/Activities:
+// Activity 1 : Arithmetic Operations
+// • Task 1: Write a program to add two numbers and log the result to the console.
+// • Task 2:
+// Write a program to subtract two numbers and log the result to the console.
+// • Task 3: Write a program to multiply two numbers and log the result to the console.
+// • Task 4.
+// • Write a program to divide two numbers and log the result to the console.
+// • Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+// Activity 2: Assignment Operators
+// • Task 6.
+// • Use the += operator to add a number to a variable and log the result to the console.
+// • Task 7: Use the
+// -z operator to subtract a number from a variable and log the result to the console.
+// Activity 3: Comparison Operators
+// • Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+// • Task 9.
+// • Write a program to compare two numbers using and <= and log the result to the console.
+// • Task 10: Write a program to compare two numbers using
+// and
+// and log the result to the console.
+// Activity 4: Logical Operators
+// • Task 1 1: Write a program that uses the && operator to combine two conditions and log the result to the console.
+// • Task 12: Write a program that uses the I I operator to combine two conditions and log the result to the console.
+// • Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+// Activity 5: Ternary Operator
+// • Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+// ACTIVITY 1: ARITHMETIC OPERATIONS
+// TASK 1:
+let num1 = 12;
+let num2 = 13;
+const sum = num1 + num2;
+console.log(sum);
+
+// TASK 2:
+const sub = num2 - num1;
+console.log(sub);
+
+// TASK 3:
+const multi = num1 * num2;
+console.log(multi);
+
+// TASK 4:
+const divd = num2 / num1;
+console.log(divd);
+
+// TASK 5:
+
+const rem = num2 % num1;
+console.log(rem);
+
+// ACTIVITY 2: Assignment Operators
+
+// TASK 1:
+let numOfItems = 1;
+numOfItems += 2;
+console.log(numOfItems);
+
+// TASK 2:
+let numOfStudents = 200;
+numOfStudents -= 10;
+console.log(numOfStudents);
+
+// Activity 3: Comparision Operators
+
+// Task 8:
+console.log(num1 > num2);
+console.log(num2 > num1);
+
+// TASK 9:
+console.log(num1 >= num2);
+console.log(num2 >= num1);
+
+// TASK 10:
+
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 === 10);
+
+// ACTIVITY 4: Logical Operators
+
+// TASK 11:
+console.log(1 == 1 && 1 > 2);
+console.log(1 < 2 && 3 > 2);
+
+// TASK 12:
+
+console.log(1 == 1 || 1 > 2);
+console.log(1 < 2 || 3 > 2);
+console.log(1 != 1 || 10 < 0);
+
+// TASK 13:
+
+console.log(!(1 === 1));
+
+// ACTIVITY 5: Ternary Opertator
+
+// TASK 14:
+let num = 1;
+let isPositive = num >= 0 ? "Yes" : "No";
+console.log(isPositive);
